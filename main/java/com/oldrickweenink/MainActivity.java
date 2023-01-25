56a97a121c9540e6675f7b5cdc8390ff12e3efbf
Oldrick Weenink <o.weenink@hotmail.com>
2023-01-25 17:32:30 +0100

    Implemented layout for the main activity: 3 buttons for 3 difficulties, each with a high-score label. References to buttons were made in MainActivity.java

--- main/java/com/oldrickweenink/MainActivity.java
+++ main/java/com/oldrickweenink/MainActivity.java
@@ -3,12 +3,23 @@ package com.oldrickweenink;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_beginner;
+    Button btn_intermediate;
+    Button btn_advanced;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get references to buttons
+        btn_beginner = findViewById(R.id.btn_beginner);
+        btn_intermediate = findViewById(R.id.btn_intermediate);
+        btn_advanced = findViewById(R.id.btn_advanced);
+
+
     }
 }
